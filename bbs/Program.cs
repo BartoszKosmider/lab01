21e118b49d756e39a75af796c5352c991ab416fb
BartoszKosmider <bartekkosmider123@gmail.com>
2021-11-07 22:54:22 +0100

    aaa

--- bbs/Program.cs
+++ bbs/Program.cs
@@ -4,150 +4,144 @@ using System.IO;
 using System.Linq;
 using System.Numerics;
 using System.Text;
+using Microsoft.VisualBasic;
 
 namespace bbs
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            while (true)
-            {
-	            Console.WriteLine($"1 - generate key, 2 - crypt string, 3 - decrypt string");
-	            var input = Console.ReadLine();
-	            switch (input)
-	            {
-		            case "1":
-		            {
-			            GenerateKey();
-			            break;
-		            }
-		            case "2":
-		            {
-			            EncryptFile();
-			            break;
-		            }
-		            case "3":
-		            {
-			            DecryptFile();
-			            break;
-		            }
-		            default:
-		            {
-			            Console.WriteLine("incorrect input");
-			            break;
-		            }
-	            }
-            }
-        }
-        private static void EncryptFile()
-        {
-	        var key = File.ReadAllText(keyFileName);
-	        Console.WriteLine("type value to crypt");
-	        var valueToCrypt = Console.ReadLine();
-	        Console.WriteLine(valueToCrypt);
-	        byte[] bytes = Encoding.ASCII.GetBytes(valueToCrypt);
-	        string converted = null;
-	        for (int i = 0; i < bytes.Length; i++)
-	        {
-		        for (int j = 0; j < 8; j++)
-		        {
-			        converted += (bytes[i] & 0x80) > 0 ? "1" : "0";
-			        bytes[i] <<= 1;
-		        }
-	        }
-	        if (!string.IsNullOrEmpty(converted))
-	        {
-		        Console.WriteLine($"converted: {converted}, length: {converted.Length}");
-		        EncryptAndSave(converted, key);
-	        }
-        }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			while (true)
+			{
+				Console.WriteLine($"1 - generate key, 2 - crypt string, 3 - decrypt string");
+				var input = Console.ReadLine();
+				switch (input)
+				{
+					case "1":
+					{
+						GenerateKey();
+						break;
+					}
+					case "2":
+					{
+						EncryptFile();
+						break;
+					}
+					case "3":
+					{
+						DecryptFile();
+						break;
+					}
+					default:
+					{
+						Console.WriteLine("incorrect input");
+						break;
 
-        private static void DecryptFile()
-        {
-	       var key = File.ReadAllText(keyFileName);
-	       var toDecrypt = File.ReadAllText(cryptedValueFileName);
-	       
-	       if(string.IsNullOrEmpty(key) || string.IsNullOrEmpty(toDecrypt))
-		       return;
-	       
-	       var result = "";
-	       for (int i = 0; i < toDecrypt.Length; i++){
-		       if (toDecrypt[i] == '0' && key[i] == '0' || (toDecrypt[i] == '0' && key[i] == '0'))
-			       result += "1";
-		       else
-			       result += "0";
-	       }
-	       Console.WriteLine($"xor: {result}, length: {result.Length} ");
+					}
+				}
+			}
+		}
+		private static void EncryptFile()
+		{
+			var key = File.ReadAllText(keyFileName);
+			Console.WriteLine("type value to encrypt");
+			var valueToCrypt = Console.ReadLine();
+			Console.WriteLine(valueToCrypt);
+			byte[] bytes = Encoding.ASCII.GetBytes(valueToCrypt);
+			string converted = null;
+			for (int i = 0; i < bytes.Length; i++)
+			{
+				for (int j = 0; j < 8; j++)
+				{
+					converted += (bytes[i] & 0x80) > 0 ? "1" : "0";
+					bytes[i] <<= 1;
+				}
+			}
+			if (!string.IsNullOrEmpty(converted))
+			{
+				EncryptAndSave(converted, key);
+			}
+		}
 
-	       var converted = "";
-	       var bytes = GetBytesFromBinaryString(result);
-	       for (int i = 0; i < bytes.Length; i++)
-	       {
-		      //na 8 bitow i convert pojeedynczego znakux
-	       }
-	       Console.WriteLine(converted);
-        }
-        
-        private static void EncryptAndSave(string toEncrypt, string key)
-        {
-	        File.Delete(cryptedValueFileName);
-	        for (int i = 0; i < toEncrypt.Length; i++){
-		        if (toEncrypt[i] == '0' && key[i] == '0' || (toEncrypt[i] == '0' && key[i] == '0'))
-			        File.AppendAllText(cryptedValueFileName, "0");
-		        else
-			        File.AppendAllText(cryptedValueFileName, "1");
-	        }
-        }
+		private static void DecryptFile()
+		{ 
+			var key = File.ReadAllText(keyFileName); 
+			var toDecrypt = File.ReadAllText(cryptedValueFileName);
+			var xorValue = "";
+			var result = "";
 
-        private static Byte[] GetBytesFromBinaryString(String binary)
-        {
-	        var list = new List<Byte>();
+			if (string.IsNullOrEmpty(key) || string.IsNullOrEmpty(toDecrypt))
+				return;
 
-	        for (int i = 0; i < binary.Length; i += 8)
-	        {
-		        String t = binary.Substring(i, 8);
-		        var bit = Convert.ToByte(t, 2);
-		        Console.WriteLine($"t: {t}, bit: {bit}");
-		        list.Add(bit);
-	        }
+			for (int i = 0; i < toDecrypt.Length; i++)
+			{
+				if (toDecrypt[i] == '0' && key[i] == '0' || (toDecrypt[i] == '1' && key[i] == '1'))
+					xorValue += "0";
+				else
+					xorValue += "1";
+			}
+			//Console.WriteLine($"xor: {xorValue}, length: {xorValue.Length} ");
 
-	        return list.ToArray();
-        }
-        
-        private static void GenerateKey()
-        {
-	        File.Delete(keyFileName);
-	        //BigInteger p = 30000000091;
-	        //BigInteger q = 40000000003;
-	        BigInteger p = 100002979;
-	        BigInteger q = 100003243;
-	        //BigInteger p = 100000004483;
-	        //BigInteger q = 100000004987;
-	        var M = p * q;
-	        BigInteger N = 20000;
-	        //BigInteger seed = 4882516701;
-	        BigInteger seed = new Random().Next(0, Int32.MaxValue);
-	        Console.WriteLine($"input values: \n p: {p}, q: {q}, M: {M}, N: {N}, seed: {seed}\noutput value:");
-	        var count0 = 0;
-	        var count1 = 0;
+			for (int i = 0; i < xorValue.Length; i += 8)
+			{
+				var oneSign = xorValue.Substring(i, 8);
+				var intValue = Convert.ToInt32(oneSign, 2);
+				result += (char)intValue;
+			}
+			Console.WriteLine($"decrypted value: {result}");
+			File.Delete(decryptedValueFileName);
+			File.AppendAllText(decryptedValueFileName, result);
+		}
 
-	        for (int i = 0; i < N; i++)
-	        {
-		        seed = seed * seed % M;
-		        var bit = seed % 2;
-		        if (bit == 0)
-			        count0++;
-		        else
-			        count1++;
+		private static void EncryptAndSave(string toEncrypt, string key)
+		{
+			File.Delete(cryptedValueFileName);
+			var result = "";
+			for (int i = 0; i < toEncrypt.Length; i++)
+			{
+				if (toEncrypt[i] == '0' && key[i] == '0' || (toEncrypt[i] == '1' && key[i] == '1'))
+					result += "0";
+				else
+					result += "1";
+			}
+			File.AppendAllText(cryptedValueFileName, result);
+			//Console.WriteLine($"result {result}");
+		}
+		
+		private static void GenerateKey()
+		{
+			File.Delete(keyFileName);
+			//BigInteger p = 30000000091;
+			//BigInteger q = 40000000003;
+			BigInteger p = 100002979;
+			BigInteger q = 100003243;
+			//BigInteger p = 100000004483;
+			//BigInteger q = 100000004987;
+			var M = p * q;
+			BigInteger N = 20000;
+			//BigInteger seed = 4882516701;
+			BigInteger seed = new Random().Next(0, Int32.MaxValue);
+			Console.WriteLine($"input values: \n p: {p}, q: {q}, M: {M}, N: {N}, seed: {seed}");
+			var count0 = 0;
+			var count1 = 0;
 
-		        File.AppendAllText(keyFileName, bit.ToString());
-		        //Console.Write(bit);
-	        }
-	        Console.WriteLine($"\nnumber of 0: {count0} \nnumber of 1: {count1}");
-        }
-        private static readonly string keyFileName = "./../output.txt";
-        private static readonly string cryptedValueFileName = "./../crypted.txt";
+			for (int i = 0; i < N; i++)
+			{
+				seed = seed * seed % M;
+				var bit = seed % 2;
+				if (bit == 0)
+					count0++;
+				else
+					count1++;
 
-    }
+				File.AppendAllText(keyFileName, bit.ToString());
+			}
+			Console.WriteLine($"\nnumber of 0: {count0} \nnumber of 1: {count1}");
+		}
+		private static readonly string keyFileName = "./../output.txt";
+		private static readonly string cryptedValueFileName = "./../encrypted.txt";
+		private static readonly string decryptedValueFileName = "./../decrypted.txt";
+
+	}
 }
--- bbs/Tests/BbsTests.cs
+++ bbs/Tests/BbsTests.cs
@@ -1,118 +1,119 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Numerics;
 using NUnit.Framework;
 
 namespace bbs
 {
-    public class BbsTests
-    {
-        [SetUp]
-        public void onStart()
-        {
-            inputValue = File.ReadAllText(fileName);
-            Console.WriteLine(inputValue.Length);
+	public class BbsTests
+	{
+		[SetUp]
+		public void onStart()
+		{
+			inputValue = File.ReadAllText(fileName);
+			Console.WriteLine(inputValue.Length);
 
-            if (inputValue.Length >= length)
-                inputValue = inputValue[..length];
-            else
-                throw new Exception("too short input string");
-        }
+			if (inputValue.Length >= length)
+				inputValue = inputValue[..length];
+			else
+				throw new Exception("too short input string");
+		}
 
-        [Test]
-        public void testFips140_2()
-        {
-            var count1 = inputValue.Count(c => c == '1');
+		[Test]
+		public void testFips140_2()
+		{
+			var count1 = inputValue.Count(c => c == '1');
 
-            Assert.IsTrue(count1 is > 9725 and < 10275);
-            Assert.AreEqual(length, inputValue.Length);
-        }
+			Assert.IsTrue(count1 is > 9725 and < 10275);
+			Assert.AreEqual(length, inputValue.Length);
+		}
 
-        [Test]
-        public void testSerii()
-        {
-            int[] streak0 = { 0, 0, 0, 0, 0, 0 };
-            int[] streak1 = { 0, 0, 0, 0, 0, 0 };
-            var count = 1;
+		[Test]
+		public void testSerii()
+		{
+			int[] streak0 = { 0, 0, 0, 0, 0, 0 };
+			int[] streak1 = { 0, 0, 0, 0, 0, 0 };
+			var count = 1;
 
-            for (int i = 0; i < length-1; i++)
-            {
-                if (inputValue[i] == inputValue[i + 1] && count < 6)
-                    count++;
-                else if(inputValue[i] == '0')
-                {
-                    streak0[count - 1]++;
-                    count = 1;
-                }
-                else if (inputValue[i] == '1')
-                {
-                    streak1[count - 1]++;
-                    count = 1;
-                }
-            }
-            Console.WriteLine($"{streak0[0]},{streak0[1]},{streak0[2]},{streak0[3]},{streak0[4]},{streak0[5]} ");
-            Console.WriteLine($"{streak1[0]},{streak1[1]},{streak1[2]},{streak1[3]},{streak1[4]},{streak1[5]} ");
-            Assert.IsTrue(streak0[0] is > 2315 and < 2685);
-            Assert.IsTrue(streak1[0] is > 2315 and < 2685);
-            Assert.IsTrue(streak0[1] is > 1114 and < 1386);
-            Assert.IsTrue(streak1[1] is > 1114 and < 1386);
-            Assert.IsTrue(streak0[2] is > 527 and < 723);
-            Assert.IsTrue(streak1[2] is > 527 and < 723);
-            Assert.IsTrue(streak0[3] is > 240 and < 384);
-            Assert.IsTrue(streak1[3] is > 240 and < 384);
-            Assert.IsTrue(streak0[4] is > 103 and < 209);
-            Assert.IsTrue(streak1[4] is > 103 and < 209);
-            Assert.IsTrue(streak0[5] is > 103 and < 209);
-            Assert.IsTrue(streak1[5] is > 103 and < 209);
-        }
+			for (int i = 0; i < length-1; i++)
+			{
+				if (inputValue[i] == inputValue[i + 1] && count < 6)
+					count++;
+				else if(inputValue[i] == '0')
+				{
+					streak0[count - 1]++;
+					count = 1;
+				}
+				else if (inputValue[i] == '1')
+				{
+					streak1[count - 1]++;
+					count = 1;
+				}
+			}
+			Console.WriteLine($"{streak0[0]},{streak0[1]},{streak0[2]},{streak0[3]},{streak0[4]},{streak0[5]} ");
+			Console.WriteLine($"{streak1[0]},{streak1[1]},{streak1[2]},{streak1[3]},{streak1[4]},{streak1[5]} ");
+			Assert.IsTrue(streak0[0] is > 2315 and < 2685);
+			Assert.IsTrue(streak1[0] is > 2315 and < 2685);
+			Assert.IsTrue(streak0[1] is > 1114 and < 1386);
+			Assert.IsTrue(streak1[1] is > 1114 and < 1386);
+			Assert.IsTrue(streak0[2] is > 527 and < 723);
+			Assert.IsTrue(streak1[2] is > 527 and < 723);
+			Assert.IsTrue(streak0[3] is > 240 and < 384);
+			Assert.IsTrue(streak1[3] is > 240 and < 384);
+			Assert.IsTrue(streak0[4] is > 103 and < 209);
+			Assert.IsTrue(streak1[4] is > 103 and < 209);
+			Assert.IsTrue(streak0[5] is > 103 and < 209);
+			Assert.IsTrue(streak1[5] is > 103 and < 209);
+		}
 
-        [Test]
-        public void testDlugiejSerii()
-        {
-            int count = 1;
-            int max = 0;
-            for (int i = 0; i < length - 1; i++)
-            {
-                if (inputValue[i] == inputValue[i + 1])
-                {
-                    count++;
-                    max = count > max ? count : max;
-                }
-                else
-                {
-                    count = 1;
-                }
-            }
-            Console.WriteLine($"Longest streak: {max}");
-            Assert.IsTrue(max < 26);
-        }
+		[Test]
+		public void testDlugiejSerii()
+		{
+			int count = 1;
+			int max = 0;
+			for (int i = 0; i < length - 1; i++)
+			{
+				if (inputValue[i] == inputValue[i + 1])
+				{
+					count++;
+					max = count > max ? count : max;
+				}
+				else
+				{
+					count = 1;
+				}
+			}
+			Console.WriteLine($"Longest streak: {max}");
+			Assert.IsTrue(max < 26);
+		}
 
-        [Test]
-        public void popopopokertest()
-        {
-            int[] countConverted = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-            for (int i = 0; i < length; i += 4)
-            {
-                var segment = inputValue.Substring(i, 4);
-                //Console.WriteLine($"{segment}, {i}");
-                var convertedValue = Convert.ToInt32(segment, 2);
-                countConverted[convertedValue]++;
-            }
-            Assert.AreEqual(5000, countConverted.Sum());
+		[Test]
+		public void popopopokertest()
+		{
+			int[] countConverted = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+			for (int i = 0; i < length; i += 4)
+			{
+				var segment = inputValue.Substring(i, 4);
+				//Console.WriteLine($"{segment}, {i}");
+				var convertedValue = Convert.ToInt32(segment, 2);
+				countConverted[convertedValue]++;
+			}
+			Assert.AreEqual(5000, countConverted.Sum());
 
-            for (int i = 0; i < 16; i++)
-            {
-                //Console.WriteLine($"value: {i}, count: {countConverted[i]}");
-                countConverted[i] *= countConverted[i];
-            }
+			for (int i = 0; i < 16; i++)
+			{
+				//Console.WriteLine($"value: {i}, count: {countConverted[i]}");
+				countConverted[i] *= countConverted[i];
+			}
 
-            double result = 16.0 / 5000.0 * countConverted.Sum() - 5000;
-            //Console.WriteLine($"result: {result}");
-            Assert.IsTrue(result is > 2.16 and < 46.17);
-        }
+			double result = 16.0 / 5000.0 * countConverted.Sum() - 5000;
+			//Console.WriteLine($"result: {result}");
+			Assert.IsTrue(result is > 2.16 and < 46.17);
+		}
 
-        private readonly int length = 20000;
-        private string inputValue;
-        private readonly string fileName = "./../../../binTest/output.txt";
-    }
+		private readonly int length = 20000;
+		private string inputValue;
+		private readonly string fileName = "./../../../binTest/output.txt";
+	}
 }
